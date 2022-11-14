dac54172cf9d278fb1911af759af9e0a3d14072d
Steeve VERDIEU <pixelsplats@gmail.com>
2022-11-14 07:39:43 -0500

    refactor: Change views methods to CBV
    Changed all the existing views to class based views and change the relatd routes as well.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from blog.views import ArticlesListView, ArticleDetailView
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('articles/<int:article_id>', views.show, name='show'),
+    path('', ArticlesListView.as_view(), name='index'),
+    path('articles/<int:pk>', ArticleDetailView.as_view(), name='show'),
 ]
