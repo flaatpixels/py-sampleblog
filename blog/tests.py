c6a82e806d58139075b11500143c945d3073a5e9
Steeve VERDIEU <pixelsplats@gmail.com>
2022-11-22 07:38:09 -0500

    test(blog): Add test for index listing
    Add test that verifies that the index page list the right articles in the right order

--- blog/tests.py
+++ blog/tests.py
@@ -1,2 +1,24 @@
 from django.test import TestCase
+from .models import Article
+from .views import ArticlesListView
+from django.urls import reverse
 
+class ArticlesListViewTests(TestCase):
+
+    def test_get_latest_articles(self):
+        """
+        get_latest_articles() returns all the specified number of articles
+        in reverse chornological order
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No articles are available.")
+
+        articles = []
+        for i in range(50):
+           article = Article(title = "article " + str(i + 1), content = "article content " + str(i + 1))
+           article.save()
+           articles.append(article)
+        
+        response = self.client.get('/')
+        self.assertQuerysetEqual(response.context['articles'], reversed(articles[-15:]))
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,9 @@ class ArticlesListView(ListView):
     template_name = 'blog/index.html'
     context_object_name = 'articles'
 
+    def get_queryset(self):
+        return Article.objects.order_by('-publish_at')[:15]
+
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'blog/show.html'
