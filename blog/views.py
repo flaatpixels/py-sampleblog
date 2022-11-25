5db6444d3e2e113b4a6d7e3e4b9ba7a16c6f1e67
Steeve VERDIEU <pixelsplats@gmail.com>
2022-11-25 05:53:49 -0500

    feat: Add pagination
    Add pination to list the articles.

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,13 @@ from .models import Article
 from django.views.generic import ListView, DetailView
 
 class ArticlesListView(ListView):
+    paginate_by = 15
     model = Article
     template_name = 'blog/index.html'
     context_object_name = 'articles'
 
     def get_queryset(self):
-        return Article.objects.order_by('-publish_at')[:15]
+        return Article.objects.order_by('-publish_at')
 
 class ArticleDetailView(DetailView):
     model = Article
